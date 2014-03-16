e5940549d9b5c8cf31b65d37197a09b0136b3fce
alamtopan <alamtopan@alamtopan-HP-Pavilion-14-Notebook-PC.(none)>
2014-03-15 21:31:32 -0400

    push

--- ototown/app/controllers/backend/users_controller.rb
+++ ototown/app/controllers/backend/users_controller.rb
@@ -1,49 +1,7 @@
-class Backend::UsersController < Backend::ApplicationController
+class Backend::UsersController < Backend::ResourcesController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   prepend_before_filter :draw_password, only: :update
-  def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new    
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = 'user was successfully saved'
-      redirect_to backend_user_path(@user)
-    else
-      render action: 'new'
-    end
-  end
-
-  def show
-    
-  end
-
-  def edit    
-  end
-
-  def update
-    if @user.update(user_params)
-      flash[:success] = 'user was successfully updated'
-      redirect_to edit_backend_user_path(@user)
-    else
-      render action: 'edit'
-    end
-  end
-
-  def destroy
-    if @user.destroy
-      flash[:success] = 'user was successfully removed'
-    else
-      flash[:error] = 'Failed to remove user'
-    end
-
-    redirect_to backend_users_path
-  end
+ 
 
   private
     def set_user
