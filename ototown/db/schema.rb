bf619a9d7f0378ebe8077bafdbd29730effd4e22
alamtopan <alamtopani@yahoo.com>
2014-03-26 14:23:58 -0400

    push

--- ototown/db/schema.rb
+++ ototown/db/schema.rb
@@ -227,7 +227,7 @@ ActiveRecord::Schema.define(version: 20140326152839) do
   end
 
   create_table "sessions", force: true do |t|
-    t.string   "session_id", default: "ototown_sesssion_", null: false
+    t.string   "session_id", null: false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
