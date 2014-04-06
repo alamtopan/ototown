4a9e09c399d69035d45dc2a4a879af7200336432
alamtopan <alamtopani@yahoo.com>
2014-04-06 19:53:31 -0400

    push

--- ototown/app/controllers/backend/users_controller.rb
+++ ototown/app/controllers/backend/users_controller.rb
@@ -1,6 +1,7 @@
 class Backend::UsersController < Backend::ApplicationController
   defaults :resource_class => User, :collection_name => 'users', :instance_name => 'user'
   prepend_before_filter :draw_password, only: :update
+  before_filter :sex
 
   def add_dealer
     @user = User.find(params[:id])
@@ -19,5 +20,10 @@ class Backend::UsersController < Backend::ApplicationController
       %w(password password_confirmation).each do |attr|
         params[:user].delete(attr)
       end if params[:user] && params[:user][:password].blank?
+    end
+
+    def sex
+      sex = ['Laki-Laki', 'Perempuan']
+      @sex = sex.map{|sex| [sex, sex]}
     end
 end
